5dd56c7d53f9d8fc460be5757873e0b47fcc62ab
Emmabyrne36 <emmabyrne36@gmail.com>
2018-05-03 10:53:09 +0100

    Final code

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -6,12 +6,19 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+        [StringLength(60, MinimumLength =3),Required]
         public string Title { get; set; }
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
+
+        [Display(Name = "Release Date"),DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"[A-Z]+[a-zA-Z""'\s-]*$"),Required,StringLength(30)]
         public string Genre { get; set; }
+
+        [Range(1, 100),DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"[A-Z]+[a-zA-Z""'\s-]*$"),Required,StringLength(5)]
         public string Rating { get; set; }
 
         // Old code for displaying the date for ReleaseDate
